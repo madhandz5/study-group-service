4aea1b42f2df1abcc34a0a26aa9a5f95b2f051e5
Ryan Oh <madhandz5@gmail.com>
2020-09-11 17:49:41 +0900

    Project Initialized

---
+++ src/main/java/com/studywara/StudywaraApplication.java
@@ -0,0 +1,13 @@
+package com.studywara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudywaraApplication.class, args);
+    }
+
+}
