c588f678f0792dbc59dd1c1b568ef3895229c9dc
Ryan Oh <madhandz5@gmail.com>
2020-09-21 17:51:01 +0900

    비밀번호 찾기

--- src/main/java/com/studywara/account/AccountService.java
+++ src/main/java/com/studywara/account/AccountService.java
@@ -101,7 +101,7 @@ public class AccountService implements UserDetailsService {
     }
 
     public void updateNotifications(Account account, Notifications notifications) {
-        modelMapper.map(notifications,account);
+        modelMapper.map(notifications, account);
         accountRepository.save(account);
     }
 
@@ -109,5 +109,14 @@ public class AccountService implements UserDetailsService {
         account.setNickname(nickname);
         accountRepository.save(account);
         login(account);
+    }
+
+    public void sendLoginLink(Account account) {
+        account.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(account.getEmail());
+        mailMessage.setSubject("파티구함, 로그인 링크");
+        mailMessage.setText("/login-by-email?token=" + account.getEmailCheckToken() + "&email=" + account.getEmail());
+        javaMailSender.send(mailMessage);
     }
 }
